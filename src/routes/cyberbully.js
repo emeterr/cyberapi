a5bfc234aa3a8be05584d780f38a3b2abc3e657c
Your Namepythonvista <you@example.comnablintech@gmail.com>
2023-05-29 03:24:17 +0100

    updated

--- src/routes/cyberbully.js
+++ src/routes/cyberbully.js
@@ -6,30 +6,10 @@ const { v4: uuidv4 } = require('uuid');
 router.post('/detect', async (req, res) => {
   try {
     const data = await detectIntent('en', req.body.word, uuidv4());
-    console.log(data)
-    if (data[0].queryResult.fulfillmentMessages[1]?.payload) {
-      let fields = data[0].queryResult.fulfillmentMessages[1].payload.fields;
-      res.json({
-        msg: fields.msg.stringValue,
-        isCyber: fields.msg.stringValue,
-        name: fields.msg.stringValue,
-      });
-    } else if (data[0].queryResult.fulfillmentMessages[0]?.payload) {
-        let fields = data[0].queryResult.fulfillmentMessages[0].payload.fields;
-      res.json({
-        msg: fields.msg.stringValue,
-        isCyber: fields.msg.stringValue,
-        name: fields.msg.stringValue,
-      });
-    } else {
-      res.json({
-        isCyber: false,
-        msg: 'Has no cyberbullying',
-        name: 'none',
-      });
-    }
+    let data2 = JSON.parse(data[0].queryResult.fulfillmentText)
+    res.json(data2)
   } catch (err) {
-    console.log(err);
+    // console.log(err);
     res.json(err);
   }
 });
